72a0b593ab5c85f91e1380f63c2dc2b6d228970c
ofri1 <ofrire.1@gmail.com>
2020-07-07 16:51:31 +0300

    Updates

---
+++ precache-manifest.8239fc99e9fe23fce7dd7dc7cf03001f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f8db61a08166c2dcc42bc9c6b05b6f",
+    "url": "/pfvisualizer/index.html"
+  },
+  {
+    "revision": "4b58959047d6c906663f",
+    "url": "/pfvisualizer/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e62b840c5e52d32ecf29",
+    "url": "/pfvisualizer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4b58959047d6c906663f",
+    "url": "/pfvisualizer/static/js/2.77e047a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pfvisualizer/static/js/2.77e047a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e62b840c5e52d32ecf29",
+    "url": "/pfvisualizer/static/js/main.ffba0ca4.chunk.js"
+  },
+  {
+    "revision": "f159c57af53f6003e839",
+    "url": "/pfvisualizer/static/js/runtime-main.aa74ae06.js"
+  }
+]);
